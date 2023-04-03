9c0acdbd9e8391c67a7a3dbf78f1430a82bad047
paytonshafer <paytonshafer@gmail.com>
2023-04-03 08:00:20 -0400

    update settings

--- university/university/settings.py
+++ university/university/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'univdb'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'university.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'university',
+        'USER': 'root',
+        'PASSWORD': 'mypassword', #change to your password to run
+        'HOST': 'localhost'
     }
 }
 
