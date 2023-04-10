071b0d3b47ac14ae292eee1a94d206f3244f42e1
paytonshafer <paytonshafer@gmail.com>
2023-04-10 10:49:28 -0400

    added F6

--- university/univdb/urls.py
+++ university/univdb/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('admin/salary_stats', views.salary_stats, name='salary_stats'),
     path('', views.login, name='login'),  # login page
     path('instructor/', views.instructor, name='instructor'),
-    path('student/', views.student, name='student'),
+    path('student/', views.index_student, name='student'),
+    path('student/courses', views.course_offerings, name='courses'),
 ]
--- university/univdb/views.py
+++ university/univdb/views.py
@@ -9,15 +9,51 @@ from django.template import loader
 from .models import Dept, Instructor
 
 
+mydb = mysql.connector.connect(
+  host="128.153.13.175", 
+  port= 3306,
+  user="group_b",
+  passwd='PayJefJosLog',
+  auth_plugin='mysql_native_password',
+  database="university_group_b",
+)
+
+#get cursor for database
+mycursor = mydb.cursor()
+
+# Create your views here.
+def index_student(request):  # Admin
+    template = loader.get_template('univdb/student/form.html')
+    context = {}
+
+    return HttpResponse(template.render(context, request))
+
+def course_offerings(request):
+    year = request.GET.get('year')
+    semester = request.GET.get('semester')
+    #SELECT dept, section.course_id, title, sec_id, building, room, credits from section join course on section.course_id = course.course_id where year = 2019 and semester = 1 order by dept;
+    if semester == 'fall':
+        sql = 'SELECT dept_name, section.course_id, title, sec_id, building, room, credits from section join course on section.course_id = course.course_id where year = '  + year + ' and semester = 1 order by dept_name;'
+    else: #semester == 'spring'
+        sql = 'SELECT dept_name, section.course_id, title, sec_id, building, room, credits from section join course on section.course_id = course.course_id where year = '  + year + ' and semester = 2 order by dept_name;'
+
+    mycursor.execute(sql) #execte sql query on db instance
+    data = mycursor.fetchall() #get all results
+    
+    template = loader.get_template('univdb/student/course_offerings.html')
+    context = {
+        'year': year,
+        'sem': semester,
+        'rows': data,
+    }
+
+    return HttpResponse(template.render(context, request))
+
 def login(request):
     return HttpResponse("Hello, world.\n")
 
 
 def instructor(request):
-    return HttpResponse("Hello, world.\n")
-
-
-def student(request):
     return HttpResponse("Hello, world.\n")
 
 
